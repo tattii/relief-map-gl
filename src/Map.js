faa02a71fe7a2c06070968f7f6fb4d92aa4e3b4e
tattii <yuta.tatti@gmail.com>
2019-01-06 22:51:34 +0900

    add relief

--- src/Map.js
+++ src/Map.js
@@ -13,6 +13,42 @@ export default class Map extends Component {
       zoom: 10,
       hash: true
     });
+
+    this.map.on('load', () => {
+      this.map.addSource('gsi-dem', {
+        "type": "raster-dem",
+        "encoding": "gsi",
+        "tiles": [
+			    "https://cyberjapandata.gsi.go.jp/xyz/dem_png/{z}/{x}/{y}.png"
+        ],
+        "tileSize": 256,
+	    	"maxzoom": 14,
+		    "attribution": '<a href="https://maps.gsi.go.jp/development/ichiran.html#dem" target="_blank">地理院標高タイル</a>'
+      });
+      this.map.addLayer({
+        "id": "relief",
+        "source": "gsi-dem",
+        "type": "relief",
+        "paint": {
+          "relief-opacity": 0.7,
+          "relief-gradation": true,
+          "relief-colors": [
+            0, "#2db4b4",
+            100, "#71b42d",
+            300, "#b4a72d",
+            1000, "#b4562d",
+            2000, "#b4491b",
+            4000, "#b43d09",
+            null, "#b43d09"
+          ]
+        }
+      }, 'waterway-river-canal-shadow');
+      this.map.addLayer({
+        "id": "GSI dem",
+        "source": "gsi-dem",
+        "type": "hillshade",
+      }, 'relief');
+    });
   }
 
 
